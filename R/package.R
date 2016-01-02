fb74680f867d97200d3bcd43f2a8a91548035e93
Yihui Xie <xie@yihui.name>
2016-01-02 16:43:19 -0600

    make it easy to unregister S3 methods

--- R/package.R
+++ R/package.R
@@ -7,3 +7,14 @@
 #' @aliases printr-package
 #' @importFrom knitr asis_output kable knit_print
 NULL
+
+# unregister S3 methods in this package
+unregister_S3 = function() {
+  if (!('knitr' %in% loadedNamespaces())) return()
+  objs = ls(asNamespace('printr'))
+  s3env = getFromNamespace('.__S3MethodsTable__.', 'knitr')
+  rm(list = intersect(objs, ls(s3env)), envir = s3env)
+}
+
+# remove S3 methods when the package is unloaded
+.onUnload = function(libpath) unregister_S3()
